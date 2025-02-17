65c795c317037c2977c6c6e8986f7ddf83f0a6c3
micarollo <rollomicaela@gmail.com>
2025-02-17 15:20:37 +0100

    hero

---
+++ src/main/java/com/swingy/model/Hero.java
@@ -0,0 +1,19 @@
+package main.java.com.swingy.model;
+
+public class Hero {
+    private String p_name;
+    private String p_heroClass;
+    private int p_level;
+    private int p_experiencie;
+    private int p_attack;
+    private int p_defense;
+    private int p_hitPoints;
+
+    private Weapon p_weapon;
+    private Armor p_armor;
+    private Helm p_helm;
+
+    public Hero(String name, String heroClass) {
+        
+    }
+}
