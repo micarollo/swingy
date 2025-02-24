524db6b1eecc8cc1f3355c97832895b1d8ced3ea
Micaela Rollo <rollomicaela@gmail.com>
2025-02-24 15:11:52 +0100

    clean

--- swingy/src/main/java/com/swingy/App.java
+++ swingy/src/main/java/com/swingy/App.java
@@ -14,26 +14,8 @@ public class App
         }
         if (args[0].equals("console")) 
         {
-            // Warrior warrior = new Warrior("Conan");
-            // System.out.println("Hero created: " + warrior.getName());
-            // System.out.println("Class: " + warrior.getHeroClass());
-            // System.out.println("Level: " + warrior.getLevel());
-            // System.out.println("Attack: " + warrior.getAttack());
-            // System.out.println("Defense: " + warrior.getDefense());
-            // System.out.println("Hit Points: " + warrior.getHitPoints());
-            // System.out.println("-------------------------");
-
-            // System.out.println("Conan gains 1500 XP...");
-            // warrior.gainExperience(1500);
-
-            // System.out.println("After gaining XP:");
-            // System.out.println("Level: " + warrior.getLevel());
-            // System.out.println("Attack: " + warrior.getAttack());
-            // System.out.println("Defense: " + warrior.getDefense());
-            // System.out.println("Hit Points: " + warrior.getHitPoints());
             Hero hero = HeroCreator.createHero();
             System.out.println("You have chosen: " + hero.getName());
-            // System.out.println("Hero Stats - HP: " + hero.getHitPoints() + ", Attack: " + hero.getAttack() + ", Defense: " + hero.getDefense());
             hero.displayStats();
         }
         else
