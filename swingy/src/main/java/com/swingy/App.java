c2c0992c9814920fd0470dc52d160f25958398c3
Micaela Rollo <rollomicaela@gmail.com>
2025-02-26 17:36:46 +0100

    add: MapController, GameController

--- swingy/src/main/java/com/swingy/App.java
+++ swingy/src/main/java/com/swingy/App.java
@@ -1,8 +1,8 @@
 package com.swingy;
 
 import com.swingy.controller.HeroController;
+import com.swingy.controller.MapController;
 import com.swingy.model.Hero;
-import com.swingy.model.Map;
 import com.swingy.view.ConsoleView;
 
 public class App 
@@ -17,12 +17,15 @@ public class App
         if (args[0].equals("console")) 
         {
             ConsoleView consoleView = new ConsoleView();
-            HeroController heroController = new HeroController(consoleView);
+            MapController mapController = new MapController();
+            mapController.createMap(1);
+            int size = mapController.getSize();
+            HeroController heroController = new HeroController(consoleView, mapController, size / 2, size / 2);
             Hero hero = heroController.HeroCreator();
             consoleView.displayHeroStats(hero);
             // consoleView.displayMenu();
-            Map map = new Map(hero.getLevel());
-            consoleView.displayMap(map);
+            // Map map = new Map(hero.getLevel());
+            consoleView.displayMap(mapController.getMap());
         }
         else
             System.out.println("Must be: console o gui");
--- swingy/src/main/java/com/swingy/model/Map.java
+++ swingy/src/main/java/com/swingy/model/Map.java
@@ -1,20 +1,14 @@
 package com.swingy.model;
 
-import java.util.Random;
-
 public class Map {
-    private int[][] map;
-    private int size;
-    private int maxVillians;
-    // private int heroX;
-    // private int heroY;
+    private final int[][] gridMap;
+    private final int size;
+    private final int maxVillians;
 
     public Map(int level) {
         this.size = calculateMap(level);
         this.maxVillians = calculateVillains();
-        this.map = new int[size][size];
-        generateMap();
-        placeHero();
+        this.gridMap = new int[size][size];
     }
 
     public int calculateMap(int level) {
@@ -25,46 +19,22 @@ public class Map {
         return (int) (size * size * 0.1);
     }
 
-    public void generateMap() {
-        Random random = new Random();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                map[i][j] = 0;
-            }
-        }
-        int placedVillains = 0;
-        while (placedVillains < maxVillians) {
-            int x = random.nextInt(size);
-            int y = random.nextInt(size);
-
-            if (map[x][y] == 0) {
-                map[x][y] = 1;
-                placedVillains++;
-            }
-        }
-    }
-
-    private void placeHero() {
-        x = size / 2;
-        y = size / 2;
-        map[x][y] = 2;
-    }
-
     public int getCell(int x, int y) {
         if (x >= 0 && x < size && y >= 0 && y < size)
-            return map[x][y];
+            return gridMap[x][y];
         return -1;
+    }
+
+    public void setCell(int x, int y, int value) {
+        if (x >= 0 && x < size && y >= 0 && y < size)
+            gridMap[x][y] = value;
     }
 
     public int getSize() {
         return size;
     }
 
-    // public int getHeroX() {
-    //     return heroX;
-    // }
-
-    // public int getHeroY() {
-    //     return heroY;
-    // }
+    public int getMaxVillains() {
+        return maxVillians;
+    }
 }
