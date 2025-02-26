49d37fcd4be76fa0d34a1c88aed0133b6bf82ac9
Micaela Rollo <rollomicaela@gmail.com>
2025-02-26 18:16:09 +0100

    add: gameloop from gamecontroller, modify: move coords

--- src/main/java/com/swingy/App.java
+++ src/main/java/com/swingy/App.java
@@ -1,9 +1,6 @@
 package com.swingy;
 
-import com.swingy.controller.HeroController;
-import com.swingy.controller.MapController;
-import com.swingy.model.Hero;
-import com.swingy.view.ConsoleView;
+import com.swingy.controller.GameController;
 
 public class App 
 {
@@ -16,16 +13,18 @@ public class App
         }
         if (args[0].equals("console")) 
         {
-            ConsoleView consoleView = new ConsoleView();
-            MapController mapController = new MapController();
-            mapController.createMap(1);
-            int size = mapController.getSize();
-            HeroController heroController = new HeroController(consoleView, mapController, size / 2, size / 2);
-            Hero hero = heroController.HeroCreator();
-            consoleView.displayHeroStats(hero);
-            // consoleView.displayMenu();
-            // Map map = new Map(hero.getLevel());
-            consoleView.displayMap(mapController.getMap());
+            GameController gameController = new GameController();
+            gameController.startGame();
+            // ConsoleView consoleView = new ConsoleView();
+            // MapController mapController = new MapController();
+            // mapController.createMap(1);
+            // int size = mapController.getSize();
+            // HeroController heroController = new HeroController(consoleView, mapController, size / 2, size / 2);
+            // Hero hero = heroController.HeroCreator();
+            // consoleView.displayHeroStats(hero);
+            // // consoleView.displayMenu();
+            // // Map map = new Map(hero.getLevel());
+            // consoleView.displayMap(mapController.getMap());
         }
         else
             System.out.println("Must be: console o gui");
--- src/main/java/com/swingy/controller/GameController.java
+++ src/main/java/com/swingy/controller/GameController.java
@@ -1,49 +1,53 @@
 package com.swingy.controller;
 
-import com.swingy.view.ConsoleView;
 import com.swingy.model.Hero;
+import com.swingy.view.ConsoleView;
 
 public class GameController {
     private final MapController mapController;
     private final ConsoleView consoleView;
     private final HeroController heroController;
+    private Hero hero;
 
     public GameController() {
         this.consoleView = new ConsoleView();
         this.mapController = new MapController();
+        mapController.createMap(1);
         this.heroController = new HeroController(consoleView, mapController);
     }
 
     public void startGame() {
-        mapController.createMap(1);
-        int size = mapController.getSize();
-        Hero hero = heroController.HeroCreator();
+        // int size = mapController.getSize();
+        hero = heroController.HeroCreator();
         consoleView.displayHeroStats(hero);
         consoleView.displayMap(mapController.getMap());
-
+        gameLoop();
     }
 
     public void gameLoop() {
         while (true) { 
             int input = consoleView.displayMenu();
             handleInput(input);
+            consoleView.displayMap(mapController.getMap());
         }
     }
 
     public void handleInput(int input) {
         switch (input) {
             case 1:
-                heroController.moveHero(0, -1);
-                break;
-            case 2:
-                heroController.moveHero(0, 1);
-                break;
-            case 3:
-                heroController.moveHero(1, 0);
-                break;
-            case 4:
                 heroController.moveHero(-1, 0);
                 break;
+            case 2:
+                heroController.moveHero(1, 0);
+                break;
+            case 3:
+                heroController.moveHero(0, 1);
+                break;
+            case 4:
+                heroController.moveHero(0, -1);
+                break;
+            case 5:
+                consoleView.displayHeroStats(hero);
             default:
                 throw new AssertionError();
         }
