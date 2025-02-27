c945b20cdc5b581df33db3d6cad6dc8fc0bf161c
micarollo <rollomicaela@gmail.com>
2025-02-27 11:45:45 +0100

    add: villain

--- src/main/java/com/swingy/model/Villain.java
+++ src/main/java/com/swingy/model/Villain.java
@@ -1,5 +1,13 @@
-// package com.swingy.model;
+package com.swingy.model;
 
-// public class Villain extends Character {
-    
-// }
+public abstract class Villain extends Character {
+    private String villainClass;
+
+    public Villain(String name, int level, int attack, int defense, int hitPoints) {
+        super(name, level, attack, defense, hitPoints);
+    }
+
+    public String getVillainClass() {
+        return villainClass;
+    }
+}
