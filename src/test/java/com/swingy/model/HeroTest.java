c886b728cc07ffc56621870ae44e6c9df2a93552
Micaela Rollo <rollomicaela@gmail.com>
2025-03-12 12:49:24 +0100

    test: herotest

--- src/test/java/com/swingy/model/HeroTest.java
+++ src/test/java/com/swingy/model/HeroTest.java
@@ -1,78 +1,54 @@
-// package com.swingy.model;
+package com.swingy.model;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// public class HeroTest {
-//     private Hero hero;
+class HeroTest{
+    private Hero hero;
 
-//     @BeforeEach
-//     public void setUp() {
-//         hero = new Warrior("Test Hero", 1, 10, 5, 100, 0, 0, 0);
-//     }
+    @BeforeEach
+    public void setUp() {
+        hero = new Warrior("TestHero", 1, 10, 5, 100, 0, 5, 5);
+    }
 
-//     @Test
-//     public void testInitialHeroAttributes() {
-//         assertEquals("Test Hero", hero.getName());
-//         assertEquals(1, hero.getLevel());
-//         assertEquals(10, hero.getAttack());
-//         assertEquals(5, hero.getDefense());
-//         assertEquals(100, hero.getHitPoints());
-//         assertEquals(0, hero.getExperience());
-//         assertEquals(0, hero.getX());
-//         assertEquals(0, hero.getY());
-//     }
+    @Test
+    public void testGainExperience() {
+        hero.gainExperience(500);
+        assertEquals(500, hero.getExperience());
+    }
 
-//     @Test
-//     public void testGainExperience() {
-//         hero.gainExperience(500);
-//         assertEquals(500, hero.getExperience());
-//     }
+    @Test
+    public void testCalculateLevelUp() {
+        int requiredXP = hero.calculateLevelUp(5);
+        assertEquals(12200, requiredXP);
+    }
 
-//     @Test
-//     public void testLevelUp() {
-//         hero.gainExperience(1500);
-//         if (hero.needToLevelUp()) {
-//             int requiredXP = hero.calculateLevelUp(hero.getLevel());
-//             hero.levelUp(requiredXP);
-//             assertEquals(2, hero.getLevel());
-//         }
-//     }
+    @Test
+    public void testNeedToLevelUp_False() {
+        assertFalse(hero.needToLevelUp());
+    }
 
-//     @Test
-//     public void testEquipWeapon() {
-//         Weapon sword = new Weapon(5);
-//         hero.equipArtifact(sword);
-//         assertEquals(sword, hero.getWeapon());
-//     }
+    @Test
+    public void testNeedToLevelUp_True() {
+        hero.gainExperience(1500);
+        assertTrue(hero.needToLevelUp());
+    }
 
-//     @Test
-//     public void testEquipArmor() {
-//         Armor plateArmor = new Armor(3);
-//         hero.equipArtifact(plateArmor);
-//         assertEquals(plateArmor, hero.getArmor());
-//     }
+    @Test
+    public void testLevelUp() {
+        hero.gainExperience(2600);
+        hero.levelUp(2450);
+        assertEquals(150, hero.getExperience());
+    }
 
-//     @Test
-//     public void testEquipHelm() {
-//         Helm ironHelm = new Helm(2);
-//         hero.equipArtifact(ironHelm);
-//         assertEquals(ironHelm, hero.getHelm());
-//     }
-
-//     @Test
-//     public void testEquipMultipleArtifacts() {
-//         Weapon sword = new Weapon(5);
-//         Armor plateArmor = new Armor(3);
-//         Helm ironHelm = new Helm(2);
-        
-//         hero.equipArtifact(sword);
-//         hero.equipArtifact(plateArmor);
-//         hero.equipArtifact(ironHelm);
-        
-//         assertEquals(sword, hero.getWeapon());
-//         assertEquals(plateArmor, hero.getArmor());
-//         assertEquals(ironHelm, hero.getHelm());
-//     }
-// }
+    @Test
+    public void testSetAndGetPosition() {
+        hero.setX(10);
+        hero.setY(15);
+        assertEquals(10, hero.getX());
+        assertEquals(15, hero.getY());
+    }
+}
